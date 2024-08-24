03a27599e8ad6dde95166db8aa1f758e39154a01
mayowa Bernard <bernardmayowa17@gmail.com>
2024-08-24 15:21:32 +0100

    chnaged from credentials to headers

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,30 +5,35 @@ import util from "util";
 
 // User must be authenticated
 const protect = asyncHandler(async (req, res, next) => {
-  // Read JWT from the 'accessToken' cookie
-  const token = req.cookies.accessToken;
 
-  if (!token) {
+    // Read JWT from the 'jwt' cookie
+    const testToken = req.headers.authorization;
+    let token;
+  
+    if (testToken && testToken.startsWith('Bearer')) {
+      token = testToken.split(' ')[1];
+    }
+  
+    if(!token) {
       res.status(401);
-      throw new Error("Not authorized, login required");
-  }
-
-  try {
-      const decoded = await util.promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-      const user = await User.findById(decoded.userId).select('-password');
-      if (!user) {
-          res.status(401);
-          throw new Error("User does not exist");
-      }
-
-      req.user = user;
-      next();
-  } catch (error) {
+      throw new Error("Not authorized, login required")
+    }
+  
+    const decoded = await util.promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  
+    const user = await User.findById(decoded.userId).select('-password');
+    if(!user) {
       res.status(401);
-      throw new Error("Not authorized, token failed");
-  }
-});
+      throw new Error("User does not exist")
+    }
+  
+  
+    await user.save();
+  
+    req.user = user;
+    next();
+  
+  });
 
 // User must be an admin
 const admin = (req, res, next) => {
