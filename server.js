453d333fe989b6cd2d72d7976bd8fd652ee14987
maximus <bernardmayowaa@gmail.com>
2024-09-21 19:19:23 +0000

    testing socket second time

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ io.on('connection', (socket) => {
         console.log('🔥: A user disconnected:', socket.id);
         // Update users list
         users = users.filter(user => user.socketID !== socket.id);
-        io.emit('loggedIn', users);
+        socket.on('loggedIn', users);
         console.log(`User: ${users} has successfully logged in`.magenta)
     });
 
