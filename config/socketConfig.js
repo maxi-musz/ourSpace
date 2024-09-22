d22a5ef1afd410a51352cd595aa445377f0236a3
maximus <bernardmayowaa@gmail.com>
2024-09-22 21:15:54 +0000

    chnaged enppoints for get all chats to http

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -59,7 +59,7 @@ const socketHandlers = (io) => {
       const res = await sendMessage(data); 
 
       // Emit the formatted message to both sender and receiver
-      socket.to(receiverId).emit('message-response', res); 
+      socket.emit('message-response', res); 
       socket.emit('message-response', res);  // Emit to sender
 
       console.log(`Message successfully emitted to sender and receiver.`.green);
