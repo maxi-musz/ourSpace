20fb9f38fb1378c54ae3f98d94ba4721f8da1d7b
maximus <bernardmayowaa@gmail.com>
2024-09-24 00:05:14 +0100

    Move conversations event listener inside join-room new newwwwwww latest new

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -32,9 +32,10 @@ const socketHandlers = (io) => {
         console.log(`New socket message received`.yellow); 
         const res = await sendMessage(data);
         // Emit the message to everyone in the room except the sender
-        socket.broadcast.to(room).emit('message-response', res);
+        // socket.broadcast.to(room).emit('message-response', res);
         // Emit the message to the sender
-        socket.emit('message-response', res);
+        io.to(room).emit('message-response')
+        // socket.emit('message-response', res);
         console.log(`Message successfully emitted to sender and receiver.`.green);
       });
 
