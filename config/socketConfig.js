3fa4e1738ed5d93a990e8e7a26d51113c5cf2488
maximus <bernardmayowaa@gmail.com>
2024-10-14 12:59:03 +0100

    chnaged socket.on to socket.once

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -12,7 +12,6 @@ const socketHandlers = (io) => {
   io.on('connection', (socket) => {
     console.log(`User ${socket.id} connected`.yellow);
 
-    // Join Room Event
     socket.on('join-room', (data) => {
       const { propertyOwnerId, listingId, propertyUserId } = data;
       const room = `chat_${propertyOwnerId}_${listingId}_${propertyUserId}`;
@@ -20,74 +19,85 @@ const socketHandlers = (io) => {
 
       const clients = io.sockets.adapter.rooms.get(room);
       console.log(`Clients in room after join: `, clients);
-    });
-
-    // Event for sending messages (attached once per socket connection)
-    socket.on('send-message', async (data) => {
-      try {
-        const { propertyOwnerId, listingId, propertyUserId } = data;
-        const room = `chat_${propertyOwnerId}_${listingId}_${propertyUserId}`;
-        
-        // Ensure sender joins the room
-        socket.join(room);
-      
-        // Check if both sender and receiver are in the chatRoom
-        const clients = io.sockets.adapter.rooms.get(room);
     
-        console.log(`Clients in room: ${clients}`.yellow);
-        
-        // Save the message to the database
-        const res = await sendMessage(data);
+      // Conversations event handler
+      socket.on('conversations', async (data) => {
+        const res = await getMessagesForAListing(data);
 
-        // Emit the message to the room if valid
-        if (data.roomName === room) {
-          io.to(room).emit('message-response', { room, res });
+        if(data.roomName === room) {
+          io.to(room).emit("conversations-response", {room, res});
         }
-
-        // Check if both users are in the room
-        if (clients && clients.size === 2) {
-          console.log('Both users are in the room.'.green);
-        } else {
-          console.log('Receiver is not in the chatRoom yet.'.red);
-        }
-
-      } catch (error) {
-        console.error('Error sending message:', error);
-      }
-    });
-
-    // Typing event (attached once per socket connection)
-    socket.on('typing', (data) => {
-      const { senderName, roomName } = data;
-
-      // Emit the typing event only within the appropriate room
-      socket.broadcast.to(roomName).emit('typing-response', `${senderName} is typing`);
-    });
-
-    // New user joins (attached once per socket connection)
-    socket.on('newUser', (data) => {
-      users.push(data);
-      console.log("Updated users:", users);
-      io.emit('newUserResponse', users);
-    });
-
-    // Conversations event handler (attached once per socket connection)
-    socket.on('conversations', async (data) => {
-      const { propertyOwnerId, listingId, propertyUserId } = data;
-      const room = `chat_${propertyOwnerId}_${listingId}_${propertyUserId}`;
+      });
+    
+      //
+      socket.once('send-message', async (data) => {
+        try {
+          // Ensure sender joins the room
+          socket.join(room);
       
-      const res = await getMessagesForAListing(data);
+          // Check if both sender and receiver are in the chatRoom
+          const clients = io.sockets.adapter.rooms.get(room);
+    
+          console.log(`Clients in room: ${clients}`.yellow);
+          const res = await sendMessage(data);
 
-      if (data.roomName === room) {
-        io.to(room).emit("conversations-response", { room, res });
-      }
-    });
+          if(data.roomName === room) {
+            io.to(room).emit('message-response', {room, res});
+          }
+      
+          if (clients && clients.size === 2) {
+            console.log('Both users are in the room.'.green);
 
-    // Handle socket disconnection
-    socket.on('disconnect', () => {
-      console.log(`User ${socket.id} disconnected`.red);
+          } else {
+            console.log('Receiver is not in the chatRoom yet.'.red);
+          }
+
+        } catch (error) {
+          console.error('Error sending message:', error);
+        }
+      });
+      
+    
+      // Typing event
+      socket.on('typing', (data) => {
+        const { senderName, receiverId } = data;
+
+        if(data.roomName === room) {
+          socket.broadcast.to(room).emit('typing-response', `${senderName} is typing`);
+        }
+      });
+    
+      // New user joins
+      socket.on('newUser', (data) => {
+        users.push(data);
+        console.log("Updated users:", users);
+        io.emit('newUserResponse', users);
+      });
     });
-  });
+    
+});
+
+    
+  
+
+    // Space owner chat events
+    // socket.on('so-get-all-chats', async (data) => {
+    //   console.log("SPace owner".yellow)
+    //   const res = await spaceOwnerGetAllChats(data); 
+    //   console.log(`Emitting chats to user with ID: ${data.userId}`.blue);
+    //   socket.emit("so-get-all-chats", res);
+    // });
+    // //frontedn
+    // socket.emit('so-get-all-chats');
+
+    // // Space user chat events
+    // socket.on('su-get-all-chats', async (data) => {
+    //   const res = await spaceUserGetAllChats(data);
+    //   console.log(`Emitting chats to user with ID: ${data.userId}`.blue);
+    //   io.emit("su-get-all-chats", res);
+    // });
+
+    
 };
 
 export default socketHandlers;
